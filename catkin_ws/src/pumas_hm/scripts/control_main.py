a121cf1b5aaa469bdab077351ecc9d3212f57433
Hector290601 <robletes062901@gmail.com>
2022-05-19 10:28:32 -0500

    added no obstacle conduction launch files and algorithms

---
+++ catkin_ws/src/pumas_hm/scripts/control_main.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import rospy
+from std_msgs.msg import Float64
+from rospy.numpy_msg import numpy_msg
+from rospy_tutorials.msg import Floats
+import math
+from datetime import datetime
+now = datetime.now()
+filename = now.strftime("%d-%m-%Y-%H-%M-%S") + ".csv"
+left_lines = ""
+right_lines = ""
+steering_value = Float64(0)
+speed_value = Float64(0)
+goal_left_rho = 0
+goal_left_theta = 0
+goal_right_rho = 0
+goal_right_theta = 0
+iterator = 0
+suma_left_rho = 0
+suma_left_theta = 0
+suma_right_rho = 0
+suma_right_theta = 0
+
+def error_rho(rho_left = goal_left_rho, rho_right = goal_right_rho):
+    global goal_left_rho, goal_right_rho
+    e = ( 0.5 * (goal_left_rho - abs(rho_left)) ) + ( 0.5 * (goal_right_rho - abs(rho_right)) )
+    return round(e, 3)
+
+def error_theta(theta_left = goal_left_theta, theta_right = goal_right_theta):
+    global goal_left_theta, goal_right_theta
+    e = ( 0.5 * (goal_left_theta - abs(theta_left)) ) + ( 0.5 * (goal_right_theta - abs(theta_right)) )
+    return round(e, 3)
+
+def get_ideal_lanes():
+    global left_lines, right_lines, iterator, suma_left_rho, suma_left_theta, suma_right_rho, suma_right_theta
+    print("training")
+    if len(left_lines) == 2 and len(right_lines) == 2:
+        suma_rho_left = abs(left_lines[0])
+        suma_left_theta += abs(left_lines[1])
+        suma_rho_right = abs(right_lines[0])
+        suma_right_theta += abs(right_lines[1])
+        iterator += 1
+
+def decide():
+    global left_lines, right_lines, speed_value, steering_value, filename
+    speed_value = 0.2
+    steering_value = 0.0
+    rho_left = 0
+    theta_left = 0
+    rho_right = 0
+    theta_right = 0
+    data = []
+    sentido = ""
+    e_theta = 0
+    if len(left_lines) == 2 and len(right_lines) < 2:
+        rho_left = left_lines[0]
+        theta_left = left_lines[1]
+        theta_left = theta_left
+        e_rho = error_rho(rho_left)
+        spd = 22
+        e_theta = error_theta(theta_left)
+        sentido = "L"
+    elif len(left_lines) < 2 and len(right_lines) == 2:
+        rho_right = right_lines[0]
+        theta_right = right_lines[1]
+        theta_right = theta_right
+        spd = 22
+        e_rho = error_rho(rho_right = rho_right)
+        e_theta = - error_theta(theta_left = theta_right)
+        sentido = "R"
+    elif len(left_lines) == 2 and len(right_lines) == 2:
+        rho_left = left_lines[0]
+        theta_left = left_lines[1]
+        rho_right = right_lines[0]
+        theta_right = right_lines[1]
+        e_rho = error_rho(rho_left, rho_right)
+        e_theta = error_theta(theta_left, theta_right)
+        spd = 50
+        sentido = "C"
+    else:
+        spd = 10
+        e_theta = .08
+        e_rho = 0
+        spd_tmp = 0
+        sentido = "NA"
+    strng = e_theta
+    if strng >= 0.4:
+        strng = 0.4
+    elif strng <= -0.4:
+        strng = -0.4
+    return spd, strng
+
+
+def callback_left(msg):
+    global left_lines
+    left_lines = msg.data
+
+def callback_right(msg):
+    global right_lines
+    right_lines = msg.data
+
+def main():
+    global speed_value, steering_value, filename, iterator, goal_left_rho, goal_left_theta, goal_right_rho, goal_right_theta, suma_left_rho, suma_left_theta, suma_right_rho, suma_right_theta
+    print("INITIALIZING LANES CONTROL NODE...")
+    rospy.init_node('hardware_control', anonymous=True)
+    speed = rospy.Publisher('/speed', Float64, queue_size=10)
+    steering = rospy.Publisher('/steering', Float64, queue_size=10)
+    rospy.Subscriber("/raw_lanes_left", Floats, callback_left)
+    rospy.Subscriber("/raw_lanes_right", Floats, callback_right)
+    loop = rospy.Rate(60)
+    print("NODE INITIALIZED SUCCESFULLY")
+    while not rospy.is_shutdown() and iterator < 100:
+        get_ideal_lanes()
+        loop.sleep()
+    goal_left_theta = suma_left_theta / iterator
+    goal_left_rho = suma_left_rho / iterator
+    goal_right_theta = suma_right_theta / iterator
+    goal_right_rho = suma_right_rho / iterator
+    print("trained")
+    while not rospy.is_shutdown():
+        speed_value, steering_value = decide()
+        speed.publish(speed_value)
+        steering.publish(steering_value)
+        loop.sleep()
+    speed.publish(0.0)
+    steering.publish(0.0)
+
+main()
+
--- catkin_ws/src/pumas_hm/scripts/vision_main.py
+++ catkin_ws/src/pumas_hm/scripts/vision_main.py
@@ -10,19 +10,24 @@ import cv2
 import numpy as np
 import math
 import os
+import time
 
+start = time.time()
 lanes_to_publish_left = ""
 lanes_to_publish_right = ""
 lane_publisherL, lane_publisherR = "", ""
-
-def canny_frame(frame_gray):
-    blured_frame = cv2.GaussianBlur(frame_gray, (5, 5), 0)
-    cannied_frame = cv2.Canny(blured_frame, 70, 120)
-    return cannied_frame
-max_val = 250
-min_val = 100
-k_size_x = 5
+degrees_publisher = ""
+min_val = 33
+max_val = 112
 k_size_y = 5
+k_size_x = 5
+votes = 35
+degl = 44
+degr = 132
+tolerance1 = 10
+tolerance2 = 20
+left_rho_goal = 392
+right_rho_goal = -12
 
 
 def canny_frame(frame_gray):
@@ -32,15 +37,17 @@ def canny_frame(frame_gray):
     return cannied_frame, blured_frame
 
 def crop_frame(frame_cannied, shp):
-    polygon = np.array(
-            [
+    polygon = np.int32(
+            np.array(
                 [
-                    (0, shp[0] / 2),
-                    (0, shp[0]),
-                    (shp[1], shp[0]),
-                    (shp[1], shp[0] / 2)
+                    [
+                        (0, shp[0] / 2),
+                        (0, shp[0]),
+                        (shp[1], shp[0]),
+                        (shp[1], shp[0] / 2)
+                        ]
                     ]
-                ]
+                )
             )
     zeros = np.zeros_like(frame_cannied)
     cv2.fillPoly(zeros, polygon, 255)
@@ -63,118 +70,85 @@ def color_seg(frame_color, frame_gray, frame_interest):
     return ranged_frame
 
 def callback_raw_image(data):
-    global lanes_to_publish_left, lanes_to_publish_right, lane_publisherL, lane_publisherR, max_val, min_val, k_size_y, k_size_x
+    global lanes_to_publish_left, lanes_to_publish_right, lane_publisherL, lane_publisherR, degrees_publisher,max_val, min_val, k_size_y, k_size_x, votes, degl, degr, tolerance1, tolerance2, gui, left_rho_goal, right_rho_goal, start
     brdg = CvBridge()
     raw_frame = brdg.imgmsg_to_cv2(data)
     coppied_frame = np.copy(raw_frame)
     gray_frame = cv2.cvtColor(raw_frame, cv2.COLOR_BGR2GRAY)
-    cannied_frame, blured_frame = canny_frame(gray_frame) #10
+    cannied_frame, blured_frame = canny_frame(gray_frame) #10i
+    kernel = np.ones((2, 1), np.uint8)
+    cannied_frame = cv2.erode(cannied_frame, kernel)
     interest_frame = crop_frame(cannied_frame, raw_frame.shape) #15
-    #color_frame = color_seg(coppied_frame, gray_frame, interest_frame) #31
-    possible_lines = cv2.HoughLines(interest_frame, 1, np.pi/180, 25)
+    possible_lines = cv2.HoughLines(interest_frame, 1, np.pi/180, votes)
     linesL = []
     linesR = []
+    linesC = []
+    degrees = []
     if possible_lines is not None:
         const = 180 / math.pi
         l = 0
         r = 0
+        c = 0
         left_rho = 0
         left_theta = 0
         right_rho = 0
         right_theta = 0
+        center_rho = 0
+        center_theta = 0
         for line in possible_lines:
             theta = line[0][1]
             grad = round( theta * const, 4)
             if grad < 180:
                 rho = line[0][0]
-                if 55 < 65 and 65 < 75:
+                if degl - tolerance1 < grad and grad < degl + tolerance1:
                     left_rho += rho
                     left_theta += theta
                     l += 1
-                elif 117 < 127 and 127 < 137:
+                elif degr - tolerance1 < grad and grad < degr + tolerance1:
                     right_rho += rho
                     right_theta += theta
                     r += 1
         if l != 0:
             prom_left_rho = left_rho / l
             prom_left_theta = left_theta / l
-            """
-            a = math.cos(prom_left_theta)
-            b = math.sin(prom_left_theta)
-            x1 = a * prom_left_rho
-            y1 = b * prom_left_rho
-            pt1 = (int(x1 + 1000 * (-b)), int(y1 + 1000 * (a)))
-            pt2 = (int(x1 - 1000 * (-b)), int(y1 - 1000 * (a)))
-            cv2.line(raw_frame, pt1, pt2, (255, 0, 0), 3)
-            #"""
             linesL = [
                     prom_left_rho,
                     prom_left_theta,
                     ]
+            if degl - tolerance1 * 2 < prom_left_theta and prom_left_theta < degl + tolerance1 * 2:
+                print("degl cambiado")
+                degl = prom_left_theta
+            degrees.append(round( prom_left_theta * const, 4))
         if r != 0:
             prom_right_rho = right_rho / r
             prom_right_theta = right_theta / r
-            """
-            a = math.cos(prom_right_theta)
-            b = math.sin(prom_right_theta)
-            x1 = a * prom_right_rho
-            y1 = b * prom_right_rho
-            pt1 = (int(x1 + 1000 * (-b)), int(y1 + 1000 * (a)))
-            pt2 = (int(x1 - 1000 * (-b)), int(y1 - 1000 * (a)))
-            cv2.line(raw_frame, pt1, pt2, (0, 255, 0), 3)
-            #"""
             linesR = [
                     prom_right_rho,
                     prom_right_theta
                     ]
+            if prom_right_theta >= 100 and prom_right_theta <= 160:
+                print("degr cambiado")
+                degr = prom_right_theta
+            degrees.append(round( prom_right_theta * const, 4))
     lanes_to_publish_left = np.array(linesL, dtype=np.float32)
     lanes_to_publish_right = np.array(linesR, dtype=np.float32)
-    """
-    cv2.imshow("frame", raw_frame)
-    cv2.imshow("Canny", interest_frame)
-    cv2.imshow("Blur", blured_frame)
-    k = cv2.waitKey(1)
-    if k == ord('a'):
-        max_val += 10
-    elif k == ord('d'):
-        max_val -= 10
-    elif k == ord('w'):
-        min_val += 10
-    elif k == ord('s'):
-        min_val -= 10
-    elif k == ord('i'):
-        k_size_y += 2
-    elif k == ord('k'):
-        if k_size_y > 0:
-            k_size_y -= 2
-        else:
-            print("No es posible")
-    elif k == ord('j'):
-        if k_size_x > 0:
-            k_size_x -= 2
-        else:
-            print("No es posible")
-    elif k == ord('l'):
-        k_size_x += 2
-    elif k == ord('p'):
-        print([min_val, max_val, k_size_y, k_size_x])
-    #"""
+    degrees_to_publish = np.array(degrees, dtype=np.float32)
     lane_publisherL.publish(lanes_to_publish_left)
     lane_publisherR.publish(lanes_to_publish_right)
+    degrees_publisher.publish(degrees_to_publish)
 
 def main():
     print("INITIALIZING NODE")
-    global lanes_to_publish_left, lanes_to_publish_right, lane_publisherL, lane_publisherR
+    global lanes_to_publish_left, lanes_to_publish_right, lane_publisherL, lane_publisherR, degrees_publisher
     rospy.init_node('raw_img_subscriber', anonymous = True)
-    rospy.Subscriber('/raw_image', Image, callback_raw_image)
+    rospy.Subscriber('/camera/rgb/raw', Image, callback_raw_image)
     lane_publisherL = rospy.Publisher("/raw_lanes_left", numpy_msg(Floats), queue_size=10)
     lane_publisherR = rospy.Publisher("/raw_lanes_right", numpy_msg(Floats), queue_size=10)
+    degrees_publisher = rospy.Publisher("/combined_degrees", numpy_msg(Floats), queue_size=10)
     loop = rospy.Rate(60)
     print("NODE INITIALIZED SUCCESFULLY")
     while not rospy.is_shutdown():
         loop.sleep()
-
-    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
