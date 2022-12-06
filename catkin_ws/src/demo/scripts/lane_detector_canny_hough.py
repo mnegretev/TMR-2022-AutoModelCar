78498e5d3b2ccd8e4dc14b34d8fc2fdc9543432b
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2022-12-05 20:40:49 -0600

    Update world Add first version of lane detection demo

--- catkin_ws/src/demo/scripts/lane_detector_canny_hough.py
+++ catkin_ws/src/demo/scripts/lane_detector_canny_hough.py
@@ -7,6 +7,7 @@ is small enough.
 Detected lines are published in normal form (rho, theta) as 2-float arrays.
 rho is measured in pixels and theta in radians
 """
+import math
 import cv2
 import numpy
 import rospy
@@ -27,25 +28,49 @@ def detect_edges(img):
 
 #
 # Lane borders are always below the horizon and due to perspective,
-# they are always inside a triangle-shaped region in the bottom
+# they are always inside a trapezoid-shaped region in the bottom
 # half of the image. This function returns such region of interest.
-# NOTE: It is assumed a 640x480 image. 
 #
 def region_of_interest(img):
     triangle = numpy.array([[
-        (0, 450), 
-        (640, 450),
-        (320, 250) 
+        (                  0, int(img.shape[0]*0.97)),  #Left-Bottom vertex              *
+        (int(img.shape[1])  , int(img.shape[0]*0.97)),  #Right-Bottom vertex           *   *
+        (int(img.shape[1])  , int(img.shape[0]*0.80)),  #Right-top vertex            *       *
+        (int(img.shape[1]/2), int(img.shape[0]*0.40)),  #Center-top vertex           * * * * *
+        (                  0, int(img.shape[0]*0.80)),  #Left-top vertex
     ]])
     mask = numpy.zeros_like(img)
-    cv2.fillPoly(mask, triangle, 255)
+    cv2.fillPoly(mask, triangle, (255,255,255))
     masked_img = cv2.bitwise_and(img, mask)
-    return masked_img
+    return img[int(0.4*img.shape[0]):int(0.97*img.shape[0]) ,:,:]
+
+#
+# This function removes all lines with slopes outside the range -0.3, 0.3 rad
+#
+def filter_lines(lines):
+    filtered_lines = []
+    for i in range(len(lines)):
+        x1, y1, x2, y2 = lines[i][0]
+        theta = math.atan((y2 - y1)/(x2 - x1))
+        if (theta < -0.3 or theta > 0.3):
+            filtered_lines.append([x1, y1, x2, y2])
+    return filtered_lines if len(filtered_lines) > 0 else None
+        
 
 def callback_rgb_image(msg):
     bridge = CvBridge()
-    img = bridge.imgmsg_to_cv2(msg, 'bgr8')
-    cv2.imshow("Original RGB", img)
+    img   = bridge.imgmsg_to_cv2(msg, 'bgr8')
+    img   = img[int(0.4*img.shape[0]):int(0.97*img.shape[0]) ,:,:]
+    canny = detect_edges(img)
+    lines = cv2.HoughLinesP(canny, 2, numpy.pi/180, 80, minLineLength=80, maxLineGap=100)
+    lines = filter_lines(lines)
+    if lines is None:
+        return
+    for i in range(len(lines)):
+        l = lines[i]
+        cv2.line(img, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
+        
+    cv2.imshow("Region of interest", img)
     cv2.waitKey(10)
 
 def main():
